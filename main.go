ccaa471c6467cc3f03078562dd32ee348485d59f
TomRomeo <Tom.Romeo.Doil@gmail.com>
2022-02-19 12:47:15 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Anki Sync server written in Go 🚀")
+	})
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.File("favicon.svg")
+	})
+	r.Run("0.0.0.0:27701")
+}
